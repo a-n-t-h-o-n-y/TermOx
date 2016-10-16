002eaed58c85659034fb27c5d7534f1dfb1da0ff
Anthony Leedom <anthonym.leedom@gmail.com>
2016-10-16 17:45:22 -0500

    Modify test1

--- test/test1/main.cpp
+++ test/test1/main.cpp
@@ -1,10 +1,19 @@
 #include <mcurses/mcurses.hpp>
 
+class Click_paint_widget : public mcurses::Widget {
+public:
+	Click_paint_widget(unsigned x, unsigned y, unsigned width, unsigned height)
+	:Widget(x,y,width,height)
+	{}
+private:
+
+};
+
 int main()
 {
 	mcurses::System system;
 
-	mcurses::Widget widg(0, 0, mcurses::System::max_width, mcurses::System::max_height);
+	Click_paint_widget widg(0, 0, mcurses::System::max_width, mcurses::System::max_height);
 
 	system.set_head(&widg);
 
