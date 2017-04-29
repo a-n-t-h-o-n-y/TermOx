c2eb577d7db76c887f457ba5bdf5d5b3af1d2ba5
Anthony Leedom <anthonym.leedom@gmail.com>
2017-04-29 13:08:16 -0500

    Add namespace to Horizontal_scrollbar.hpp

--- include/widget_module/widgets/horizontal_scrollbar.hpp
+++ include/widget_module/widgets/horizontal_scrollbar.hpp
@@ -3,8 +3,12 @@
 
 #include "../layouts/horizontal_layout.hpp"
 
+namespace twf {
+
 class Horizontal_scrollbar : public Horizontal_layout {
 
 };
+
+} // namespace twf;
 
 #endif // HORIZONTAL_SCROLLBAR
