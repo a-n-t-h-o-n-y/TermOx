c7dd21e1b429af6e011ef58f4f56e1069a6d9808
Anthony Leedom <anthonym.leedom@gmail.com>
2017-09-29 12:26:04 -0500

    Add to list_demo and list widget.

--- demos/list_demo.cpp
+++ demos/list_demo.cpp
@@ -12,13 +12,43 @@ using namespace cppurses;
 int main() {
     System sys;
 
+    // Layouts
     Horizontal_layout layout;
-    auto& list = layout.make_child<List<Item>>();
+    auto& leftl = layout.make_child<Vertical_layout>();
+
+    // List Left
+    auto& list = leftl.make_child<List<Item>>();
     list.add_property("Name", [](const Item& i) { return i.name; });
     list.add_property("Value",
                       [](const Item& i) { return std::to_string(i.value); });
     list.add_property("Color", [](const Item& i) { return i.color; });
 
+    list.add_item(Item{});
+    list.add_item(Item{"Item 2", 7, "Green"});
+    list.add_item(Item{"Item 2", 7, "Green"});
+
+    // List Right
+    auto& list2 = layout.make_child<List<Item>>();
+    list2.add_property("Color", [](const Item& i) { return i.color; });
+    list2.add_property("Value",
+                       [](const Item& i) { return std::to_string(i.value); });
+    list2.add_property("Name", [](const Item& i) { return i.name; });
+
+    // Rotate Button
+    auto& rotate_button = leftl.make_child<Push_button>("Rotate Properties");
+
+    // Signals
+    list.selected.connect(slot::add_item(list2));
+    rotate_button.clicked.connect(slot::rotate_properties(list));
+
+    // Aesthetics
+    list.set_background(Color::Light_blue);
+    list.height_policy.stretch(5);
+    list2.set_background(Color::Light_gray);
+    rotate_button.set_background(Color::Gray);
+    rotate_button.set_foreground(Color::Violet);
+
+    Focus::set_focus_to(&list);
     sys.set_head(&layout);
     return sys.run();
 }
