1fad596d7403ecb8431f88cf982ca52022c8ea03
Anthony Leedom <anthonym.leedom@gmail.com>
2017-11-21 22:50:06 -0600

    Remove commented out code.

--- demos/main_menu.cpp
+++ demos/main_menu.cpp
@@ -11,11 +11,6 @@ Main_menu::Main_menu() {
 }
 
 void Main_menu::initialize() {
-    // enable_border(main_menu);
-    // main_menu.border.north_enabled = false;
-    // main_menu.border.north_west_enabled = false;
-    // main_menu.border.north_east_enabled = false;
-    // main_menu.border.north_east_enabled = false;
     hl.height_policy.type(Size_policy::Fixed);
     hl.height_policy.hint(1);
 }
