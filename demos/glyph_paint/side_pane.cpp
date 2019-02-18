169be4228e077edac91ab61000e424b5e1d45a9b
Anthony Leedom <anthonym.leedom@gmail.com>
2019-02-18 12:41:28 -0600

    Remove duplicate brush color set in glyph paint cycle box.

--- demos/glyph_paint/side_pane.cpp
+++ demos/glyph_paint/side_pane.cpp
@@ -18,11 +18,6 @@ Side_pane::Side_pane() {
     glyph_select.height_policy.preferred(6);
     color_select_stack.height_policy.fixed(3);
 
-    for (auto& child : color_select_stack.top_row.children.get()) {
-        child->brush.set_background(Color::Light_gray);
-        child->brush.set_foreground(Color::Black);
-    }
-
     show_glyph.height_policy.fixed(1);
     show_glyph.set_alignment(Alignment::Center);
 }
