e7f129c7dd61605d85dc02344e5e939130013815
Anthony Leedom <anthonym.leedom@gmail.com>
2019-05-15 14:19:12 -0500

    Add Focus_policy::Direct

--- include/cppurses/widget/focus_policy.hpp
+++ include/cppurses/widget/focus_policy.hpp
@@ -8,7 +8,8 @@ namespace cppurses {
 /** Tab: Tab key presses will cycle the focus to Widgets with this policy. */
 /** Click: A mouse click on this Widget will give it focus. */
 /** Strong: Both Tab and Click policies apply. */
-enum class Focus_policy { None, Tab, Click, Strong };
+/** Direct: Can only get focus if directly set with Focus::set_focus_to(). */
+enum class Focus_policy { None, Tab, Click, Strong, Direct };
 
 }  // namespace cppurses
 #endif  // CPPURSES_WIDGET_FOCUS_POLICY_HPP
