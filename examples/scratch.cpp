2370627e90809831bd4061323827026944589d87
Anthony Leedom <anthonym.leedom@gmail.com>
2024-01-23 22:34:06 -0800

    Add pipe operators to Glyph

--- examples/scratch.cpp
+++ examples/scratch.cpp
@@ -1,21 +1,16 @@
 #include <termox/core.hpp>
 
+using namespace ox;
+
 int main()
 {
-    using namespace ox;
-
     class {
        public:
-        // TODO maybe rename to mouse_press
         auto handle_mouse_press(Mouse const& m) -> EventResponse
         {
             if (m.button == Mouse::Button::Left) {
-                // Painter{}[m.at] = {U'X',
-                //                    {.foreground = ColorIndex::BrightBlue}};
-                Painter{}[m.at] = {U'X', {.foreground = TrueColor{0x8bb14e}}};
-                // Painter{}[m.at] = {U'X', {.foreground = DefaultColor{}}};
-                // Painter{}[m.at] = U'X' | fg(Red);
-                // TODO Color::Red?
+                Painter{}[m.at] = U'X' | Trait::Bold | Trait::Italic |
+                                  fg(ColorIndex::Red) | bg(TrueColor{0x8bb14e});
                 Terminal::cursor = m.at;
             }
             return {};
--- include/termox/glyph.hpp
+++ include/termox/glyph.hpp
@@ -2,6 +2,7 @@
 
 #include <esc/brush.hpp>
 #include <esc/color.hpp>
+#include <esc/trait.hpp>
 
 namespace ox {
 
@@ -9,6 +10,8 @@ using ::esc::Brush;
 using ::esc::Color;
 using ::esc::ColorIndex;
 using ::esc::DefaultColor;
+using ::esc::Trait;
+using ::esc::Traits;
 using ::esc::TrueColor;
 
 /**
@@ -17,32 +20,103 @@ using ::esc::TrueColor;
  * Contains a symbol and a Brush to describe how the symbol should be displayed.
  */
 struct Glyph {
-    char32_t symbol  = U'\0';
-    esc::Brush brush = {};
+    char32_t symbol = U'\0';
+    Brush brush     = {};
+
+    [[nodiscard]] constexpr bool operator==(Glyph const&) const = default;
+    [[nodiscard]] constexpr bool operator!=(Glyph const&) const = default;
 };
 
+// PIPE OPS --------------------------------------------------------------------
+
 /**
- * @brief Compares the symbol and brush of two Glyphs for equality
+ * @brief Creates a Glyph with the given symbol and background Color.
  *
- * @param lhs The left hand side of the comparison.
- * @param rhs The right hand side of the comparison.
-j* @return true if the symbol and brush are equal, false otherwise.
-*/
-[[nodiscard]] inline auto operator==(Glyph const& lhs, Glyph const& rhs) -> bool
+ * @param symbol The symbol to display.
+ * @param c The ColorBG to apply to the return Glyph's brush.
+ * @return A Glyph with the given symbol and Color background.
+ */
+[[nodiscard]] constexpr auto operator|(char32_t symbol, esc::ColorBG c) -> Glyph
 {
-    return lhs.symbol == rhs.symbol && lhs.brush == rhs.brush;
+    return {symbol, {.background = c.value}};
 }
 
 /**
- * @brief Compares the symbol and brush of two Glyphs for inequality
+ * @brief Copy the Glyph and modify its background Color.
  *
- * @param lhs The left hand side of the comparison.
- * @param rhs The right hand side of the comparison.
- * @return true if the symbol and brush are not equal, false otherwise.
+ * @param g The Glyph to work from.
+ * @param c The ColorBG to apply to the return Glyph's brush.
+ * @return The passed in Glyph with the given Color background.
  */
-[[nodiscard]] inline auto operator!=(Glyph const& lhs, Glyph const& rhs) -> bool
+[[nodiscard]] constexpr auto operator|(Glyph g, esc::ColorBG c) -> Glyph
 {
-    return !(lhs == rhs);
+    g.brush.background = c.value;
+    return g;
+}
+
+/**
+ * @brief Creates a Glyph with the given symbol and foreground Color.
+ *
+ * @param symbol The symbol to display.
+ * @param c The ColorFG to apply to the return Glyph's brush.
+ * @return A Glyph with the given symbol and Color foreground.
+ */
+[[nodiscard]] constexpr auto operator|(char32_t symbol, esc::ColorFG c) -> Glyph
+{
+    return {symbol, {.foreground = c.value}};
+}
+
+/**
+ * @brief Copy the Glyph and modify its foreground Color.
+ *
+ * @param g The Glyph to work from.
+ * @param c The ColorFG to apply to the return Glyph's brush.
+ * @return The passed in Glyph with the given Color foreground.
+ */
+[[nodiscard]] constexpr auto operator|(Glyph g, esc::ColorFG c) -> Glyph
+{
+    g.brush.foreground = c.value;
+    return g;
+}
+
+/**
+ * @brief Creates a Glyph with the given symbol and Trait.
+ *
+ * @param symbol The symbol to display.
+ * @param t The Trait to apply to the return Glyph's brush.
+ * @return A Glyph with the given symbol and Trait.
+ */
+[[nodiscard]] constexpr auto operator|(char32_t symbol, Trait t) -> Glyph
+{
+    return {symbol, {.traits = t}};
+}
+
+/**
+ * @brief Copy the Glyph and add a Trait.
+ *
+ * @param g The Glyph to work from.
+ * @param t The Trait to apply to the return Glyph's brush.
+ * @return The passed in Glyph with the given Trait added.
+ */
+[[nodiscard]] constexpr auto operator|(Glyph g, Trait t) -> Glyph
+{
+    g.brush.traits = g.brush.traits | t;
+    return g;
+}
+
+/**
+ * @brief Removes a Trait from a Glyph's brush.
+ *
+ * Use the esc::remove_trait(Trait) function to create the RemoveTrait object.
+ *
+ * @param g The Glyph to remove the Trait from.
+ * @param t The Trait to remove from the Glyph's brush.
+ * @return The Glyph with the Trait removed from its brush.
+ */
+[[nodiscard]] constexpr auto operator|(Glyph g, esc::RemoveTrait t) -> Glyph
+{
+    g.brush.traits = g.brush.traits | t;
+    return g;
 }
 
 }  // namespace ox
